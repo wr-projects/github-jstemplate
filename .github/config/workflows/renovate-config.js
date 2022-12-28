63bf30f5a79b46bf41dfeedcbe310b4c3f5c336d
GregoireF <gfavreau.wrprojects@gmail.com>
2022-12-28 22:38:54 +0100

    initial commit

---
+++ .github/config/workflows/renovate-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	migratePresets: {
+		'@wr-projets': 'local>org/utils/packages/renovate-config/src',
+	},
+	username: 'wrprojects-renovate-bot[bot]',
+	gitAuthor: 'wrprojects-renovate-bot[bot] <119612261+wrprojects-renovate-bot[bot]@users.noreply.github.com>',
+	onboarding: false,
+	platform: 'github',
+	autodiscover: true,
+}
